7965ee68bbacdb38532a6b926975b63dc6fa4b6e
Gouda <2463689@cognizant.com>
2025-12-22 04:56:05 -0800

    Added basic PrimeNG

--- ui/frontend/src/app/landing/landing.ts
+++ ui/frontend/src/app/landing/landing.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { InputTextModule } from 'primeng/inputtext';
+import {FloatLabelModule} from 'primeng/floatlabel';
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [InputTextModule, FloatLabelModule],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
+
 export class Landing {
+     
 
 }
