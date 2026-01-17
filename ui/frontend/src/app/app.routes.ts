82f7c50dc263e6db4c78da311e47428d07c6e6f9
Gouda <2463689@cognizant.com>
2026-01-17 11:31:58 +0530

    corrected analytics file structure

--- ui/frontend/src/app/app.routes.ts
+++ ui/frontend/src/app/app.routes.ts
@@ -27,7 +27,7 @@ import { AboutUs } from './shared/components/about-us/about-us';
 import { SearchFlightHotelsHomepageComponent } from './feature/Homepage/search-flight-hotels-homepage.component/search-flight-hotels-homepage.component';
 import { Footer } from './shared/components/footer/footer';
 import { FeaturesPanelHomepageComponent } from './feature/Homepage/features-panel-homepage.component/features-panel-homepage.component';
-import { AnalyticsComponent } from './feature/Homepage/analytics.component/analytics.component';
+import { AnalyticsComponent } from './feature/Analytics/analytics.component/analytics.component';
 
 
 
